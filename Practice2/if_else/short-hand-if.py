8be0b744958f68fae4aafac36d21785b83904529
AlpamysR <a_ryspai@kbtu.kz>
2026-02-01 11:33:24 +0500

    Implement short-hand if and if-else examples
    Add examples of short-hand if, if-else, and multiple conditions.

---
+++ Practice2/if_else/short-hand-if.py
@@ -0,0 +1,24 @@
+#Short if
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#Note: You still need the colon : after the condition.
+
+#Short If-Else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+#Assign a Value With If-Else
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+#Multiple Conditions on One Line
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+
