0e76583dc1b71f912e0676bc0cfe48af9ed93abe
AlpamysR <a_ryspai@kbtu.kz>
2026-02-01 11:31:45 +0500

    update

---
+++ Practice2/boolean_/boolean-operators.py
@@ -0,0 +1,20 @@
+# ==	Equal	x == y	
+# !=	Not equal	x != y	
+# >	    Greater than	x > y	
+# <	    Less than	x < y	
+# >=	Greater than or equal to	x >= y	
+# <=	Less than or equal to	x <= y
+
+x = 5
+y = 3
+print(x == y) #False
+print(x != y) #True
+print(x > y)  #True
+print(x < y)  #False
+print(x >= y) #True
+print(x <= y) #False
+
+#Complicated comparison operators
+x = 5
+print(1 < x < 10) #True
+print(1 < x and x < 10) #True
