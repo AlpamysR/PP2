50837b0f1802cf5a656c31321d3ff4a78aa2b697
AlpamysR <a_ryspai@kbtu.kz>
2026-02-01 11:31:20 +0500

    update

---
+++ Practice2/boolean_/boolean-comparison.py
@@ -0,0 +1,12 @@
+#Comparison with numbers, output boolean values
+print(10 > 9) #True
+print(10 == 9)#False
+print(10 < 9) #True
+
+#Comparing with if-else 
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")  # <-OUTPUT
+else:
+  print("b is not greater than a") 
