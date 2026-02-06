ef582ef712257f2cbb90e7585fa704e049541db5
AlpamysR <a_ryspai@kbtu.kz>
2026-02-06 12:28:00 +0500

    Implement Person class with __init__() examples
    Created a Person class with multiple __init__() method examples to demonstrate object instantiation with different parameters.

---
+++ Practice3/classes/init_method.py
@@ -0,0 +1,36 @@
+#Create a class named Person, use the __init__() method to assign values for name and age
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#With __init__(), you can set initial values when creating the object
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 28)
+
+print(p1.name)
+print(p1.age)
+
+#Multiple parameters
+class Person:
+  def __init__(self, name, age, city, country):
+    self.name = name
+    self.age = age
+    self.city = city
+    self.country = country
+
+p1 = Person("Linus", 30, "Oslo", "Norway")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
