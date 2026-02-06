c1a4fcaedc878cd65b78a92c39ca4ead0e8e411f
AlpamysR <a_ryspai@kbtu.kz>
2026-02-06 12:27:04 +0500

    Add MyClass and Person class examples
    Added basic class definitions and object manipulation examples.

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,14 @@
+#basic class assignment
+class MyClass:
+  x = 5
+
+#Create an object named p1, and print the value of x
+p1 = MyClass()
+print(p1.x)
+
+#Delete an object from class
+del p1
+
+#Avoiding error for empty classes       
+class Person:
+  pass
