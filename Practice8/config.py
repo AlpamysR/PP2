0786ee9e906cf50d2c828701caf1a124f190577c
AlpamysR <a_ryspai@kbtu.kz>
2026-03-27 10:05:24 +0500

    Refactor database configuration into DB_CONFIG dict

--- Practice8/config.py
+++ Practice8/config.py
@@ -1,5 +1,7 @@
-host = "localhost"
-user = "postgres"
-password = "15022008"
-db_name = "phonebook_db"
-port = 5432
+DB_CONFIG = {
+    "dbname": "mydb",
+    "user": "postgres", 
+    "password": "15022008",
+    "host": "localhost",
+    "port": "5432"
+}
